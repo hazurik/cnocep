c5d0d7230722d480bdd06aeb21c3a1ec0b3a5a03
hazurik <168477636+hazurik@users.noreply.github.com>
2024-08-08 12:57:20 +0300

    Update fsnoser.py

--- fsnoser.py
+++ fsnoser.py
@@ -10,4 +10,9 @@ if platform.system() == "Windows":
 else:
     os.system('clear')
 
-exec(startapp)
+answer = input("Пропустить установку нуждающихся компонентов? [y/n]: ")
+if answer.upper() == 'Y':
+    exec(startapp)
+if answer.upper() == 'N':
+    os.system('pip install requests && clear && pip install pycryptodome && clear && pip install colorama && clear && pip install telethon && clear && pip install pyfiglet && clear && pip install termcolor && clear')
+    exec(startapp)
