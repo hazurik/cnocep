563052c9ea376b2425a3d180cfa79e61533b572d
hazurik <168477636+hazurik@users.noreply.github.com>
2024-08-09 16:20:01 +0300

    Add files via upload

--- fsbox.py
+++ fsbox.py
@@ -4,6 +4,8 @@ from requests import get
 import os
 import platform
 import pystyle
+import random
+import string
 
 clr = """
 if platform.system() == "Windows":
@@ -12,6 +14,37 @@ else:
     os.system('clear')
 """
 exec(clr)
+
+mailgen = """
+def generate_email():
+    domains = ["gmail.com", "yahoo.com", "outlook.com", "icloud.com", "mail.ru", "yandex.ru", "hotmail.com"]
+    username_length = random.randint(5, 10)
+    username = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(username_length))
+    domain = random.choice(domains)
+    email = f"{username}@{domain}"
+    return email
+
+# Пример использования
+print("Сгенерированный email:", generate_email())
+"""
+
+passgen = """
+def generate_password(length):
+    if length < 1:
+        return "Длина пароля должна быть положительным числом"
+    
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for _ in range(length))
+    
+    return password
+
+# Пример использования
+length = int(input("Введите длину пароля: "))
+exec(clr)
+print("Сгенерированный пароль:", generate_password(length))
+"""
+
+
 
 pystyle.Write.Print(pystyle.Center.XCenter("""
 
@@ -29,8 +62,8 @@ pystyle.Write.Print(pystyle.Center.XCenter("""
     ║                                                        ║
     ║                Что сегодня запускаем?:                 ║
     ║                                                        ║
-    ║                       [1] FSnos                        ║
-    ║                       [2] DBHack                       ║
+    ║          [1] FSnos         [3] Генератор паролей       ║
+    ║          [2] DBHack        [4] Генератор почт          ║
     ║                                                        ║
     ╚════════════════════════════════════════════════════════╝
     ║                  Создатель: @fosertpvp                 ║
@@ -49,3 +82,10 @@ if choice == "1":
 if choice == "2":
     app1 = get("https://raw.githubusercontent.com/hazurik/cnocep/main/dbhack.py").text
     exec(app1)
+
+if choice == "3":
+    exec(passgen)
+
+if choice == "4":
+    exec(clr)
+    exec(mailgen)
